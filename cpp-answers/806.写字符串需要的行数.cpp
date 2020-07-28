9cc18e629ef51f19685e5ec5fd9a56e11245d0c1
yanghongyang <15652575067@163.com>
2020-07-28 10:55:55 +0800

    Leetcode 806.写字符串需要的行数

--- cpp-answers/806.写字符串需要的行数.cpp
+++ cpp-answers/806.写字符串需要的行数.cpp
@@ -1,15 +1,36 @@
 /*
+ * @Description: 
+ * @Author: Hongyang_Yang
+ * @Date: 2020-07-18 07:11:11
+ * @LastEditors: Hongyang_Yang
+ * @LastEditTime: 2020-07-28 10:54:19
+ */
+/*
  * @lc app=leetcode.cn id=806 lang=cpp
  *
  * [806] 写字符串需要的行数
  */
 
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    vector<int> numberOfLines(vector<int>& widths, string S) {
-
+    vector<int> numberOfLines(vector<int> &widths, string S)
+    {
+        int strSize = S.size();
+        int total = 0;
+        int lines = 1;
+        for (int i = 0; i < strSize; i++)
+        {
+            total += widths[S[i] - 'a'];
+            if (total > 100)
+            {
+                lines++;
+                total = 0;
+                i--;
+            }
+        }
+        return {lines, total};
     }
 };
 // @lc code=end
-
