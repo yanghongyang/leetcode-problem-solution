966546730df8bdcab7eeb43ee30c797cf87605fd
yanghongyang <15652575067@163.com>
2020-08-06 23:17:47 +0800

    leetcode 3.无重复字符的最长子串

---
+++ cpp-answers/3.无重复字符的最长子串.cpp
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=3 lang=cpp
+ *
+ * [3] 无重复字符的最长子串
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        vector<int> dict(256, -1);
+        int maxLen = 0, start = -1;
+        for(int i = 0; i < s.size(); i++)
+        {
+            if(dict[s[i]] > start)  
+                start = dict[s[i]];
+            dict[s[i]] = i;
+            maxLen = max(maxLen, i - start);
+        }
+        return maxLen;
+    }
+};
+// @lc code=end
+
