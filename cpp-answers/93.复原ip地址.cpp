ea2890ff44a8e799e29a1a4a00c1490d04fab037
yanghongyang <15652575067@163.com>
2020-08-09 09:05:39 +0800

    leetcode 93.复原ip地址（暴力、回溯）

--- cpp-answers/93.复原ip地址.cpp
+++ cpp-answers/93.复原ip地址.cpp
@@ -9,7 +9,7 @@ class Solution {
 public:
     //这道题一看就是...回溯法
     //回溯法万岁！
-    vector<string> restoreIpAddresses(string s) {
+/*     vector<string> restoreIpAddresses(string s) {
         vector<string> res;
         string ip;
         dfs(s, 0, 0, ip, res);
@@ -40,6 +40,56 @@ public:
             if(num == 0)
                 break;
         }
+    } */
+    //讲道理！！！这道题给的字符串长度应该不会特别长，因为要返回的是所有可能的IP地址格式
+    //那么！用暴力法也是可以考虑的！！！
+    //整个字符串应该被合理地分成4个部分，那么只要穷举每个部分然后判断是否合理就可以啦！
+    bool valid(string s)
+    {
+        //0~255意味着不超过3位
+        if(s.size() <= 0 || s.size() > 3)
+            return false;
+        //除此之外s不能以0为开头
+        if(s[0] == '0' && s.size() > 1)
+            return false;
+        //计算数的大小
+        int result = 0;
+        for(int i = 0; i < s.size(); i++)
+        {
+            result = result * 10 + s[i] - '0';
+        }
+        return (result >= 0 && result <= 255);
+    }
+    vector<string> restoreIpAddresses(string s) 
+    {
+        int n = s.size(); 
+        vector<string> res;
+        if(n > 12 || n < 4) //肯定不可能分成4个合理的部分
+            return {};
+        for(int i = 1; i < n; i++)
+        {
+            string a = s.substr(0, i);  //第一部分
+            if(valid(a))
+            {
+                for(int j = i + 1; j < n; j++)
+                {
+                    string b = s.substr(i, j - i);
+                    if(valid(b))
+                    {
+                        for(int k = j + 1; k < n; k++)
+                        {
+                            string c = s.substr(j, k - j);
+                            string d = s.substr(k, n - k);
+                            if(valid(c) && valid(d))
+                            {
+                                res.push_back(a + '.' + b + '.' + c + '.' + d);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return res;
     }
 };
 // @lc code=end
