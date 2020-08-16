30972cffe59ddcede64a11e04b68d44d86600b2d
yanghongyang <15652575067@163.com>
2020-08-16 09:26:18 +0800

    leetcode 733.图像渲染.cpp（DFS）

--- cpp-answers/733.图像渲染.cpp
+++ cpp-answers/733.图像渲染.cpp
@@ -1,15 +1,36 @@
 /*
+ * @Description: 
+ * @Author: Hongyang_Yang
+ * @Date: 2020-07-24 11:48:59
+ * @LastEditors: Hongyang_Yang
+ * @LastEditTime: 2020-08-16 09:24:16
+ */
+/*
  * @lc app=leetcode.cn id=733 lang=cpp
  *
  * [733] 图像渲染
  */
 
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
-
+    void dfs(vector<vector<int>> &image, int r, int c, int m, int n, int target, int newColor)
+    {
+        if (r < 0 || c < 0 || r == m || c == n || image[r][c] == newColor || image[r][c] != target)
+            return;
+        image[r][c] = newColor;
+        dfs(image, r + 1, c, m, n, target, newColor);
+        dfs(image, r - 1, c, m, n, target, newColor);
+        dfs(image, r, c + 1, m, n, target, newColor);
+        dfs(image, r, c - 1, m, n, target, newColor);
+    }
+    vector<vector<int>> floodFill(vector<vector<int>> &image, int sr, int sc, int newColor)
+    {
+        int m = image.size();
+        int n = image[0].size();
+        dfs(image, sr, sc, m, n, image[sr][sc], newColor);
+        return image;
     }
 };
 // @lc code=end
-
