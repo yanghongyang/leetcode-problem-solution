cb4f32d885e27e8ed909c847a778c37947c4317a
yanghongyang <15652575067@163.com>
2020-08-23 12:19:53 +0800

    lc 461（补充Brian Kernighan算法）

--- cpp-answers/461.汉明距离.cpp
+++ cpp-answers/461.汉明距离.cpp
@@ -1,8 +1,8 @@
 /*
  * @Author: your name
  * @Date: 2020-06-13 22:13:26
- * @LastEditTime: 2020-06-13 22:16:24
- * @LastEditors: your name
+ * @LastEditTime: 2020-08-23 12:19:26
+ * @LastEditors: Hongyang_Yang
  * @Description: In User Settings Edit
  * @FilePath: \undefinede:\leetcode刷题\461.汉明距离.cpp
  */
@@ -16,7 +16,8 @@
 class Solution
 {
 public:
-    int hammingDistance(int x, int y)
+    //模拟
+    /*     int hammingDistance(int x, int y)
     {
         int count = 0;
         while (x || y)
@@ -24,6 +25,18 @@ public:
             count += ((x & 1) ^ (y & 1));
             x = x >> 1;
             y = y >> 1;
+        }
+        return count;
+    } */
+    //Brian Kernighan算法
+    int hammingDistance(int x, int y)
+    {
+        int tmp = x ^ y;
+        int count = 0;
+        while (tmp)
+        {
+            count++;
+            tmp = tmp & (tmp - 1);
         }
         return count;
     }
