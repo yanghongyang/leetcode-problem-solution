82a31473f9e4d6275c725600b29bdbbc81c36971
yanghongyang <15652575067@163.com>
2020-08-29 16:56:21 +0800

    lc 200.（新增BFS方法）

--- cpp-answers/200.岛屿数量.cpp
+++ cpp-answers/200.岛屿数量.cpp
@@ -3,7 +3,7 @@
  * @Author: Hongyang_Yang
  * @Date: 2020-07-03 19:01:45
  * @LastEditors: Hongyang_Yang
- * @LastEditTime: 2020-07-04 11:38:41
+ * @LastEditTime: 2020-08-29 16:55:39
  */
 /*
  * @lc app=leetcode.cn id=200 lang=cpp
@@ -44,7 +44,7 @@ public:
     }
 };*/
 //思路2：并查集
-class UnionFind
+/* class UnionFind
 {
 public:
     vector<int> parent;
@@ -128,6 +128,59 @@ private:
     int get(int i, int j)
     {
         return i * n + j;
+    }
+}; */
+class Solution
+{
+public:
+    int numIslands(vector<vector<char>> &grid)
+    {
+        int nr = grid.size();
+        if (!nr)
+            return 0;
+        int nc = grid[0].size();
+
+        int num_islands = 0;
+        for (int r = 0; r < nr; r++)
+        {
+            for (int c = 0; c < nc; c++)
+            {
+                if (grid[r][c] == '1')
+                {
+                    num_islands++;
+                    grid[r][c] = '0';
+                    queue<pair<int, int>> neighbors;
+                    neighbors.push({r, c});
+                    while (!neighbors.empty())
+                    {
+                        auto rc = neighbors.front();
+                        neighbors.pop();
+                        int row = rc.first, col = rc.second;
+                        if (row - 1 >= 0 && grid[row - 1][col] == '1')
+                        {
+                            neighbors.push({row - 1, col});
+                            grid[row - 1][col] = '0';
+                        }
+                        if (row + 1 < nr && grid[row + 1][col] == '1')
+                        {
+                            neighbors.push({row + 1, col});
+                            grid[row + 1][col] = '0';
+                        }
+                        if (col - 1 >= 0 && grid[row][col - 1] == '1')
+                        {
+                            neighbors.push({row, col - 1});
+                            grid[row][col - 1] = '0';
+                        }
+                        if (col + 1 < nc && grid[row][col + 1] == '1')
+                        {
+                            neighbors.push({row, col + 1});
+                            grid[row][col + 1] = '0';
+                        }
+                    }
+                }
+            }
+        }
+        return num_islands;
     }
 };
 // @lc code=end
