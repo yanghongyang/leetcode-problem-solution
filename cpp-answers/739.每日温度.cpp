bdf8b8461b7d4c0e77abe26e6f4f45afe3eaf86e
yanghongyang <15652575067@163.com>
2020-09-04 13:34:42 +0800

    lc 739.（单调栈）

--- cpp-answers/739.每日温度.cpp
+++ cpp-answers/739.每日温度.cpp
@@ -5,11 +5,28 @@
  */
 
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    vector<int> dailyTemperatures(vector<int>& T) {
-
+    vector<int> dailyTemperatures(vector<int> &T)
+    {
+        int n = T.size();
+        vector<int> ans(n, 0);
+        if (n <= 1)
+            return ans;
+        stack<int> warmer;
+        for (int i = n - 1; i >= 0; i--)
+        {
+            while (!warmer.empty() && T[i] >= T[warmer.top()])
+            {
+                /* code */
+                warmer.pop();
+            }
+            if (!warmer.empty())
+                ans[i] = warmer.top() - i;
+            warmer.push(i);
+        }
+        return ans;
     }
 };
 // @lc code=end
-
