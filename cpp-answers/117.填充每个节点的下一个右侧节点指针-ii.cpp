1a3216833f0c154b99a6069fdc1e52c82cb71dae
yanghongyang <15652575067@163.com>
2020-09-18 16:22:11 +0800

    lc 117.（层次遍历变型）

---
+++ cpp-answers/117.填充每个节点的下一个右侧节点指针-ii.cpp
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode.cn id=117 lang=cpp
+ *
+ * [117] 填充每个节点的下一个右侧节点指针 II
+ */
+
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution
+{
+public:
+    Node *connect(Node *root)
+    {
+        if (!root)
+            return NULL;
+        queue<Node *> q;
+        q.push(root);
+        while (!q.empty())
+        {
+            int n = q.size();
+            for (int i = 0; i < n; i++)
+            {
+                Node *t = q.front();
+                q.pop();
+                if (i != n - 1)
+                {
+                    Node *curfront = q.front();
+                    t->next = curfront;
+                }
+                if (t->left)
+                    q.push(t->left);
+                if (t->right)
+                    q.push(t->right);
+            }
+        }
+        return root;
+    }
+};
+// @lc code=end
