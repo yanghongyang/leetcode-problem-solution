41565a14c7e787f875b41c9448373cc29dcafff0
yanghongyang <15652575067@163.com>
2020-09-18 16:41:17 +0800

    lc 117.（额看错题目了，这题应该用常数级空间复杂度解决，看成链表解决即可）

--- cpp-answers/117.填充每个节点的下一个右侧节点指针-ii.cpp
+++ cpp-answers/117.填充每个节点的下一个右侧节点指针-ii.cpp
@@ -26,7 +26,8 @@ public:
 class Solution
 {
 public:
-    Node *connect(Node *root)
+    //额这种方法虽然可以，但是不符合题目要求的常数级额外空间
+    /*     Node *connect(Node *root)
     {
         if (!root)
             return NULL;
@@ -49,6 +50,35 @@ public:
                 if (t->right)
                     q.push(t->right);
             }
+        }
+        return root;
+    } */
+    //思路和代码源自：https://leetcode-cn.com/problems/populating-next-right-pointers-in-each-node-ii/solution/117tian-chong-jie-dian-de-xia-yi-ge-jie-dian-you-c/
+    //主要思路：把每一层看成一个单链表，在每一层开始创建dummy节点
+    Node *connect(Node *root)
+    {
+        if (!root)
+            return NULL;
+        Node *cur = root;
+        while (cur)
+        {
+            Node *dummy = new Node(); //创建dummy结点
+            Node *tail = dummy;       //维护一个尾节点指针
+            while (cur)
+            {
+                if (cur->left)
+                {
+                    tail->next = cur->left;
+                    tail = tail->next;
+                }
+                if (cur->right)
+                {
+                    tail->next = cur->right;
+                    tail = tail->next;
+                }
+                cur = cur->next; //cur同层移动到下一个位置
+            }
+            cur = dummy->next;
         }
         return root;
     }
