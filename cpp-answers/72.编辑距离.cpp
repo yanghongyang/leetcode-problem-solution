21536d93fef81713c68f3a4c84a262f0cb1f2101
yanghongyang <15652575067@163.com>
2020-11-12 01:33:08 +0800

    经典dp，滚动数组的方法没有写

---
+++ cpp-answers/72.编辑距离.cpp
@@ -0,0 +1,48 @@
+/*
+ * @Description: 
+ * @Author: Hongyang_Yang
+ * @Date: 2020-11-12 01:20:31
+ * @LastEditors: Hongyang_Yang
+ * @LastEditTime: 2020-11-12 01:31:41
+ */
+/*
+ * @lc app=leetcode.cn id=72 lang=cpp
+ *
+ * [72] 编辑距离
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int minDistance(string word1, string word2)
+    {
+        int n = word1.size(), m = word2.size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+        for (int i = 0; i <= n; i++)
+        {
+            dp[i][0] = i;
+        }
+        for (int j = 0; j <= m; j++)
+        {
+            dp[0][j] = j;
+        }
+
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= m; j++)
+            {
+                if (word1[i - 1] == word2[j - 1])
+                {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                else
+                {
+                    dp[i][j] = min(dp[i - 1][j - 1], min(dp[i - 1][j], dp[i][j - 1])) + 1;
+                }
+            }
+        }
+        return dp[n][m];
+    }
+};
+// @lc code=end
