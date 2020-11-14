11df736e8e05442e7362144b9152928cf00b8e85
yanghongyang <15652575067@163.com>
2020-11-14 20:23:52 +0800

    心累..脑筋急转弯，只有完全平方数的因子的个数为奇数。这个Git老是玩不转，烦死了

---
+++ cpp-answers/319.灯泡开关.cpp
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=319 lang=cpp
+ *
+ * [319] 灯泡开关
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int bulbSwitch(int n)
+    {
+        return (int)sqrt(n);
+    }
+};
+// @lc code=end
