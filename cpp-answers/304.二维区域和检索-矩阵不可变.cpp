0973598a54116e56d08ac26ca3e62a67a72eb461
yanghongyang <15652575067@163.com>
2020-11-15 09:26:04 +0800

    前缀和？

---
+++ cpp-answers/304.二维区域和检索-矩阵不可变.cpp
@@ -0,0 +1,47 @@
+/*
+ * @Description: 
+ * @Author: Hongyang_Yang
+ * @Date: 2020-11-15 09:14:00
+ * @LastEditors: Hongyang_Yang
+ * @LastEditTime: 2020-11-15 09:25:41
+ */
+/*
+ * @lc app=leetcode.cn id=304 lang=cpp
+ *
+ * [304] 二维区域和检索 - 矩阵不可变
+ */
+
+// @lc code=start
+class NumMatrix
+{
+private:
+    int row, col;
+    vector<vector<int>> sums;
+
+public:
+    NumMatrix(vector<vector<int>> &matrix)
+    {
+        row = matrix.size();
+        col = row > 0 ? matrix[0].size() : 0;
+        sums = vector<vector<int>>(row + 1, vector<int>(col + 1, 0));
+        for (int i = 1; i <= row; i++)
+        {
+            for (int j = 1; j <= col; j++)
+            {
+                sums[i][j] = matrix[i - 1][j - 1] + sums[i - 1][j] + sums[i][j - 1] - sums[i - 1][j - 1];
+            }
+        }
+    }
+
+    int sumRegion(int row1, int col1, int row2, int col2)
+    {
+        return sums[row2 + 1][col2 + 1] - sums[row2 + 1][col1] - sums[row1][col2 + 1] + sums[row1][col1];
+    }
+};
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix* obj = new NumMatrix(matrix);
+ * int param_1 = obj->sumRegion(row1,col1,row2,col2);
+ */
+// @lc code=end
