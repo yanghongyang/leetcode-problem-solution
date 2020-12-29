81c99ddeac4d99d854b545e3a9e1cc2ce72a466e
yanghongyang <15652575067@163.com>
2020-12-29 17:56:04 +0800

    模拟/两个hash表可以解决问题

---
+++ cpp-answers/30.串联所有单词的子串.cpp
@@ -0,0 +1,53 @@
+/*
+ * @Description: 
+ * @Author: Hongyang_Yang
+ * @Date: 2020-12-29 17:04:26
+ * @LastEditors: Hongyang_Yang
+ * @LastEditTime: 2020-12-29 17:54:43
+ */
+/*
+ * @lc app=leetcode.cn id=30 lang=cpp
+ *
+ * [30] 串联所有单词的子串
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    vector<int> findSubstring(string s, vector<string> &words)
+    {
+        vector<int> ans;
+        if (s.size() <= 0 || words.size() < 0 || words[0].size() < 0)
+            return ans;
+        unordered_map<string, int> cnt;
+        for (auto word : words)
+        {
+            cnt[word]++;
+        }
+        int n = s.size();
+        int wl = words[0].size();
+        int len = wl * words.size();
+        for (int i = 0; i < n - len + 1; i++)
+        {
+            unordered_map<string, int> seen;
+            int j = 0;
+            for (; j < words.size(); j++)
+            {
+                string word = s.substr(i + j * wl, wl); //在s的i+j*wl处取长度为wl的子串
+                if (cnt.find(word) != cnt.end())        //如果在cnt中可以找到
+                {
+                    seen[word]++;               //在seen数组中该word数++
+                    if (seen[word] > cnt[word]) //如果当前seen[word]个数超过了cnt[word]，也就是超过了words中该word出现的次数，则失败
+                        break;
+                }
+                else //找不到的话就肯定是失败，跳出循环
+                    break;
+            }
+            if (j == words.size())
+                ans.push_back(i);
+        }
+        return ans;
+    }
+};
+// @lc code=end
