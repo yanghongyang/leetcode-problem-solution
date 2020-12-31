2bac480f67ab1c0ffeef7f6c6fa3be3e2f8e3e9e
Hongyang Yang <15652575067@163.com>
2020-12-31 22:15:20 +0800

    数据结构的综合应用

--- cpp-answers/1331.数组序号转换.cpp
+++ cpp-answers/1331.数组序号转换.cpp
@@ -1,15 +1,39 @@
 /*
+ * @Description: 
+ * @Author: Hongyang_Yang
+ * @Date: 2020-07-24 21:21:17
+ * @LastEditors: Hongyang_Yang
+ * @LastEditTime: 2020-12-31 22:01:14
+ */
+/*
  * @lc app=leetcode.cn id=1331 lang=cpp
  *
  * [1331] 数组序号转换
  */
 
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-    vector<int> arrayRankTransform(vector<int>& arr) {
-
+    vector<int> arrayRankTransform(vector<int> &arr)
+    {
+        int n = arr.size();
+        map<int, vector<int>> ans;
+        for (int i = 0; i < n; i++)
+        {
+            ans[arr[i]].push_back(i);
+        }
+        vector<int> res(n, 0);
+        int index = 1;
+        for (auto x : ans)
+        {
+            for (auto y : x.second)
+            {
+                res[y] = index;
+            }
+            index++;
+        }
+        return res;
     }
 };
 // @lc code=end
-
