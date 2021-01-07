9a0fa902dc75d4691f0d2bbbf1ccb0286de9329d
Hongyang Yang <15652575067@163.com>
2021-01-07 11:46:40 +0800

    滑动窗口

--- cpp-answers/3.无重复字符的最长子串.cpp
+++ cpp-answers/3.无重复字符的最长子串.cpp
@@ -1,11 +1,18 @@
 /*
+ * @Description: 
+ * @Author: Hongyang_Yang
+ * @Date: 2020-08-06 23:12:28
+ * @LastEditors: Hongyang_Yang
+ * @LastEditTime: 2021-01-07 11:13:08
+ */
+/*
  * @lc app=leetcode.cn id=3 lang=cpp
  *
  * [3] 无重复字符的最长子串
  */
 
 // @lc code=start
-class Solution {
+/* class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         vector<int> dict(256, -1);
@@ -19,6 +26,31 @@ public:
         }
         return maxLen;
     }
+}; */
+class Solution
+{
+public:
+    int lengthOfLongestSubstring(string s)
+    {
+        int n = s.size();
+        if (n <= 0)
+            return 0;
+        int l = 0, r = 1;
+        int ans = 0;
+        vector<int> cnt(256, 0);
+        cnt[s[l]]++;
+        while (l < n)
+        {
+            while (cnt[s[r]] < 1 && r < n)
+            {
+                cnt[s[r]]++;
+                r++;
+            }
+            ans = max(ans, r - l);
+            cnt[s[l]]--;
+            l++;
+        }
+        return ans;
+    }
 };
 // @lc code=end
-
